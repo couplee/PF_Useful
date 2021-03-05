0e18c5e2767bad6e2e33d9751482e6ed47eea4c6
couplee <couplee888@gmail.com>
2021-03-05 13:51:52 +0000

    [Add] いいね機能非同期通信

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,15 @@
 class LikesController < ApplicationController
   def create
-    product = Product.find(params[:product_id])
-    like = current_user.likes.new(product_id: product.id)
+    @product = Product.find(params[:product_id])
+    like = current_user.likes.new(product_id: @product.id)
     like.save
-    redirect_to product_path(product)
+    # redirect_to product_path(product)            <!--非同期通信5/5-->
   end
   
   def destroy
-    product = Product.find(params[:product_id])
-    like = current_user.likes.find_by(product_id: product.id)
+    @product = Product.find(params[:product_id])
+    like = current_user.likes.find_by(product_id: @product.id)
     like.destroy
-    redirect_to product_path(product)
+    # redirect_to product_path(product)            <!--非同期通信5/5-->
   end
 end
